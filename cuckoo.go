fdc4102bad424a2f67d33e015203bdc7ab9a47e6
Braden Walker <bradenawalker@gmail.com>
2019-02-10 22:35:05 -0800

    Tons of refactor, allow varying f and b, and add NewRaw constructor

--- cuckoo.go
+++ cuckoo.go
@@ -34,9 +34,14 @@ import (
 )
 
 type Filter struct {
-	inner bitarray.BitArray
-	count int
-	full  bool
+	inner      bitarray.BitArray
+	count      int
+	overflowed bool
+
+	// The number of bits per fingerprint.
+	f int
+
+	bucketEncoding bucketEncoding
 }
 
 type Result byte
@@ -48,9 +53,22 @@ const (
 	Maybe
 )
 
+func (r Result) String() string {
+	switch r {
+	case No:
+		return "No"
+	case Maybe:
+		return "Maybe"
+	default:
+		return fmt.Sprintf("Unknown(%d)", r)
+	}
+}
+
 // Returns a new filter capable of holding n items with an estimated false-positive rate of fp.
 // If more than n items are added, the false-positive rate approaches 1.
 func New(n int, fp float64) *Filter {
+	b := 4
+	f := int(math.Min(math.Max(math.Ceil(math.Log2(2*float64(b)/float64(fp))), 4), 16))
 	loadFactor := 0.95
 	if n > 1<<20 {
 		loadFactor = 0.6
@@ -58,11 +76,33 @@ func New(n int, fp float64) *Filter {
 	if n > 1<<25 {
 		loadFactor = 0.4
 	}
-	bitsPerItem := (math.Log2(1/fp) + 2) / loadFactor
-	// Round numBuckets to an even power of two, so that the xor operations work.
-	numBuckets := 1 << uint(64-bits.LeadingZeros64(uint64(float64(n)*bitsPerItem)))
+	return NewRaw(f, b, int(float64(n)/float64(b)/float64(loadFactor)))
+}
+
+// Returns a new filter.
+//
+// - f: fingerprint length in bits. [2, 16]
+//
+// - b: bucket size in number of entries. [1, 8]
+//
+// - n: number of buckets in the table.
+func NewRaw(f, b, n int) *Filter {
+	if f < 2 || f > 16 || b < 1 || b > 8 || f*b > 64 {
+		panic("invalid params")
+	}
+
+	// Round n to an even power of two, so that the xor operations work.
+	n = 1 << uint(bits.Len64(uint64(n)))
+	var enc bucketEncoding
+	if f >= 4 && b == 4 {
+		enc = packedBucketEncoding{f}
+	} else {
+		enc = directBucketEncoding{f, b}
+	}
 	return &Filter{
-		inner: bitarray.New(numBuckets, 12),
+		inner:          bitarray.New(n, uint(enc.size())),
+		f:              f,
+		bucketEncoding: enc,
 	}
 }
 
@@ -77,8 +117,11 @@ func (fl *Filter) nBuckets() uint64 {
 
 // Adds an item to the filter. After Add(x) returns, Contains(x) returns Maybe.
 func (fl *Filter) Add(x []byte) {
-	f, i1, i2 := fl.itemToIdxs(x)
 	fl.count++
+	if fl.overflowed {
+		return
+	}
+	f, i1, i2 := fl.itemToIdxs(x)
 
 	// First, attempt to add x's fingerprint to either of its candidate buckets, as long as there's
 	// room.
@@ -98,8 +141,8 @@ func (fl *Filter) Add(x []byte) {
 	maxNumKicks := 500
 	b := fl.getBucket(i)
 	for n := 0; n < maxNumKicks; n++ {
-		entry := rand.Int() % 4
-		f, b[entry] = b[entry], f
+		entry := rand.Int() % b.l
+		f, b.entries[entry] = b.entries[entry], f
 		fl.setBucket(i, b)
 		i = fl.otherIdx(f, i)
 		b = fl.getBucket(i)
@@ -112,14 +155,17 @@ func (fl *Filter) Add(x []byte) {
 		// of _that_ bucket, so loop around again.
 	}
 	// If we made it here, then we did maxNumKicks successive kicks without finding a bucket with
-	// empty space, so we should just consider the filter 'full' and return Maybe for everything
-	// from now on.
-	fl.full = true
+	// empty space, so we should just consider the filter 'overflowed' and return Maybe for
+	// everything from now on.
+	fl.overflowed = true
 }
 
 // Deletes x from the filter. x must have been previously added.
 func (fl *Filter) Delete(x []byte) {
 	fl.count--
+	if fl.overflowed {
+		return
+	}
 	f, i1, i2 := fl.itemToIdxs(x)
 
 	is := [2]uint64{i1, i2}
@@ -136,7 +182,7 @@ func (fl *Filter) Delete(x []byte) {
 
 // Returns No if x is definitely not in the filter, and Maybe if x might be in the filter.
 func (fl *Filter) Contains(x []byte) Result {
-	if fl.full {
+	if fl.overflowed {
 		return Maybe
 	}
 	f, i1, i2 := fl.itemToIdxs(x)
@@ -150,8 +196,29 @@ func (fl *Filter) Contains(x []byte) Result {
 	return No
 }
 
+func (fl *Filter) Overflowed() bool {
+	return fl.overflowed
+}
+
 func (fl *Filter) Count() int {
 	return fl.count
+}
+
+func (fl *Filter) dump() {
+	for i := uint64(0); i < fl.nBuckets(); i++ {
+		b := fl.getBucket(i)
+		fmt.Printf("%d: %08x %s\n", i, fl.inner.Get(int(i)), b)
+	}
+}
+
+func (fl *Filter) check() {
+	for i := uint64(0); i < fl.nBuckets(); i++ {
+		b := fl.getBucket(i)
+		bits := fl.inner.Get(int(i))
+		if bits != fl.bucketEncoding.encode(b) {
+			panic("bucket broken")
+		}
+	}
 }
 
 // Given x, returns x's fingerprint and the indexes of the two buckets that x's fingerprint would be
@@ -169,20 +236,19 @@ func (fl *Filter) otherIdx(f fingerprint, i1 uint64) uint64 {
 }
 
 func (fl *Filter) getBucket(i uint64) bucket {
-	var b bucket
-	b.decode(uint16(fl.inner.Get(int(i))))
-	return b
+	return fl.bucketEncoding.decode(fl.inner.Get(int(i)))
 }
 
 func (fl *Filter) setBucket(i uint64, b bucket) {
-	bits := b.encode()
-	fl.inner.Set(int(i), uint64(bits))
+	bits := fl.bucketEncoding.encode(b)
+	fl.inner.Set(int(i), bits)
 }
 
 func (fl *Filter) hashToFingerprint(hash uint64) fingerprint {
 	// Prefer the high bits of the hash, because the low bits are used for i1.
-	for shift := uint(60); shift > 0; shift -= 4 {
-		result := fingerprint((hash >> shift) & 0xF)
+	mask := (uint64(1) << uint(fl.f)) - 1
+	for shift := 64 - fl.f; shift > 0; shift -= fl.f {
+		result := fingerprint((hash >> uint(shift)) & mask)
 		if result != 0 {
 			return result
 		}
@@ -202,14 +268,93 @@ func (fl *Filter) hashFingerprint(x fingerprint) uint64 {
 	return h.Sum64()
 }
 
-// A 4-bit fingerprint of an element. 0 means 'none'.
-type fingerprint byte
-type bucket [4]fingerprint
+type bucketEncoding interface {
+	encode(b bucket) uint64
+	decode(x uint64) bucket
+	size() uint64
+}
+
+type directBucketEncoding struct {
+	f int
+	b int
+}
+
+func (e directBucketEncoding) encode(b bucket) uint64 {
+	result := uint64(0)
+	for i := 0; i < e.b; i++ {
+		result |= uint64(b.entries[i]) << uint(i*e.f)
+	}
+	return result
+}
+func (e directBucketEncoding) decode(x uint64) bucket {
+	var result bucket
+	result.l = e.b
+	mask := (uint64(1) << uint(e.f)) - 1
+	for i := 0; i < e.b; i++ {
+		result.entries[i] = fingerprint((x >> uint(i*e.f)) & mask)
+	}
+	return result
+}
+func (e directBucketEncoding) size() uint64 {
+	return uint64(e.f * e.b)
+}
+
+type packedBucketEncoding struct{ f int }
+
+func (e packedBucketEncoding) encode(b bucket) uint64 {
+	// The order of items isn't meaningful. And because the order doesn't matter, there are only
+	// 3,876 possible buckets, which is encodable in 12 bits.
+	e.sortBucketByLower4(&b)
+
+	result := uint64(0)
+	packed := uint16(0)
+	for i := 0; i < 4; i++ {
+		packed |= uint16(b.entries[i]&0xF) << uint(12-4*i)
+		result |= (uint64(b.entries[i]) >> 4) << uint((e.f-4)*i)
+	}
+	size := e.size()
+	result |= uint64(lookupFingerprintsToBits[packed]) << uint(size-12)
+	return result
+}
+
+func (e packedBucketEncoding) decode(x uint64) bucket {
+	size := e.size()
+	var b bucket
+	b.l = 4
+	packed := lookupBitsToFingerprints[uint16(x>>uint(size-12))]
+	mask := (uint64(1) << uint(e.f-4)) - 1
+	for i := 0; i < 4; i++ {
+		b.entries[i] = fingerprint((uint64(packed) >> uint(12-4*i)) & 0xf)
+		b.entries[i] |= fingerprint((x >> uint((e.f-4)*i) & mask) << 4)
+	}
+	return b
+}
+
+func (e packedBucketEncoding) size() uint64 {
+	return uint64(12 + (e.f-4)*4)
+}
+
+func (e packedBucketEncoding) sortBucketByLower4(b *bucket) {
+	for i := 3; i >= 0; i-- {
+		for j := 0; j < i; j++ {
+			if (b.entries[j] & 0xF) > (b.entries[j+1] & 0xF) {
+				b.entries[j], b.entries[j+1] = b.entries[j+1], b.entries[j]
+			}
+		}
+	}
+}
+
+// A fingerprint of an element. 0 means 'none'.
+type fingerprint uint16
+type bucket struct {
+	l       int // The length of `entries`.
+	entries [8]fingerprint
+}
 
 // Returns true if this bucket contains the given fingerprint.
 func (b bucket) contains(f fingerprint) bool {
-	for _, entry := range b {
-		if entry == f {
+	for i := 0; i < b.l; i++ {
+		if b.entries[i] == f {
 			return true
 		}
 	}
@@ -218,69 +363,47 @@ func (b bucket) contains(f fingerprint) bool {
 
 // Add one instance of the given fingerprint to this bucket.
 func (b *bucket) add(f fingerprint) {
-	for i := range b {
-		if i == 0 {
-			b[i] = f
+	for i := 0; i < b.l; i++ {
+		if b.entries[i] == 0 {
+			b.entries[i] = f
+			return
 		}
 	}
 }
 
 // Deletes one instance of the given fingerprint from this bucket.
 func (b *bucket) delete(f fingerprint) {
-	for i, f2 := range b {
-		if f == f2 {
-			b[i] = 0
+	for i := 0; i < b.l; i++ {
+		if f == b.entries[i] {
+			b.entries[i] = 0
 		}
 	}
 }
 
 // Returns true if this bucket has an empty slot.
 func (b *bucket) hasEmpty() bool {
-	for i := range b {
-		if b[i] == 0 {
+	for i := 0; i < b.l; i++ {
+		if b.entries[i] == 0 {
 			return true
 		}
 	}
 	return false
 }
 
-// Returns a 12-bit encoding of the bucket.
-func (b *bucket) encode() uint16 {
-	// sort, while mutating bucket, isn't destructive because the order of items isn't meaningful
-	b.sort()
-	// 4 fingerprints of 4 bits each fit into 16 bits.
-	packed := (uint16(b[0]) << 12) |
-		(uint16(b[1]) << 8) |
-		(uint16(b[2]) << 4) |
-		uint16(b[3])
-	// But because the order doesn't matter, there are only 3,876 possible buckets, which is
-	// encodable in 12 bits.
-	return lookupFingerprintsToBits[packed]
-}
-
-// Inverse of encode().
-func (b *bucket) decode(x uint16) {
-	packed := lookupBitsToFingerprints[x]
-	b[0] = fingerprint(packed >> 12)
-	b[1] = fingerprint((packed >> 8) & 0xf)
-	b[2] = fingerprint((packed >> 4) & 0xf)
-	b[3] = fingerprint((packed) & 0xf)
-}
-
 func (b *bucket) sort() {
-	for i := 0; i < len(b); i++ {
-		for j := i + 1; j < len(b); j++ {
-			if b[j-1] > b[j] {
-				b[j-1], b[j] = b[j], b[j-1]
+	for i := b.l - 1; i >= 0; i-- {
+		for j := 0; j < i; j++ {
+			if b.entries[j] > b.entries[j+1] {
+				b.entries[j], b.entries[j+1] = b.entries[j+1], b.entries[j]
 			}
 		}
 	}
 }
 
 func (b bucket) String() string {
-	s := make([]string, len(b))
-	for i := range b {
-		s[i] = fmt.Sprintf("%x", b[i])
+	s := make([]string, b.l)
+	for i := 0; i < b.l; i++ {
+		s[i] = fmt.Sprintf("%x", b.entries[i])
 	}
 	return strings.Join(s, ",")
 }
--- cuckoo_test.go
+++ cuckoo_test.go
@@ -1,33 +1,62 @@
 package cuckoo
 
 import (
+	"encoding/hex"
 	"math/rand"
 	"testing"
 
-	"github.com/stretchr/testify/require"
 	"github.com/bradenaw/trand"
+	"github.com/stretchr/testify/require"
 )
 
-
 func TestBucketSort(t *testing.T) {
-	b := bucket{0xF, 0x0, 0x1, 0xA}
+	b := bucket{l: 4, entries: [8]fingerprint{0xF, 0x0, 0x1, 0xA}}
 	b.sort()
-	require.Equal(t, bucket{0x0, 0x1, 0xA, 0xF}, b)
+	require.Equal(t, bucket{l: 4, entries: [8]fingerprint{0x0, 0x1, 0xA, 0xF}}, b)
+
+	b = bucket{l: 4, entries: [8]fingerprint{0x2C, 0x0F, 0x35, 0x1A}}
+	b.sort()
+	require.Equal(t, bucket{l: 4, entries: [8]fingerprint{0x0F, 0x1A, 0x2C, 0x35}}, b)
 }
 
 func TestBucketEncode(t *testing.T) {
-	b := bucket{0x0, 0xF, 0x1, 0xA}
-	bits := b.encode()
-	var b2 bucket
-	b2.decode(bits)
-	require.Equal(t, b, b2)
+	check := func(enc bucketEncoding, b bucket) {
+		bits := enc.encode(b)
+		b2 := enc.decode(bits)
+		b.sort()
+		b2.sort()
+		require.Equal(t, b, b2)
+	}
+
+	check(packedBucketEncoding{f: 4}, bucket{l: 4, entries: [8]fingerprint{0x0, 0x0, 0x0, 0x0}})
+	check(packedBucketEncoding{f: 4}, bucket{l: 4, entries: [8]fingerprint{0xA, 0x0, 0x0, 0x0}})
+	check(packedBucketEncoding{f: 4}, bucket{l: 4, entries: [8]fingerprint{0x0, 0xF, 0x1, 0xA}})
+	check(packedBucketEncoding{f: 5}, bucket{l: 4, entries: [8]fingerprint{0x1C, 0x0F, 0x15, 0x1A}})
+	check(packedBucketEncoding{f: 6}, bucket{l: 4, entries: [8]fingerprint{0x2C, 0x0F, 0x35, 0x1A}})
+	check(packedBucketEncoding{f: 8}, bucket{l: 4, entries: [8]fingerprint{0x8C, 0x7D, 0x38, 0x44}})
+
+	check(directBucketEncoding{f: 2, b: 4}, bucket{l: 4, entries: [8]fingerprint{0x0, 0x0, 0x0, 0x0}})
+	check(directBucketEncoding{f: 2, b: 4}, bucket{l: 4, entries: [8]fingerprint{0x3, 0x0, 0x0, 0x0}})
+	check(directBucketEncoding{f: 2, b: 4}, bucket{l: 4, entries: [8]fingerprint{0x0, 0x3, 0x1, 0x2}})
+	check(directBucketEncoding{f: 4, b: 4}, bucket{l: 4, entries: [8]fingerprint{0x0, 0x0, 0x0, 0x0}})
+	check(directBucketEncoding{f: 4, b: 4}, bucket{l: 4, entries: [8]fingerprint{0xA, 0x0, 0x0, 0x0}})
+	check(directBucketEncoding{f: 4, b: 4}, bucket{l: 4, entries: [8]fingerprint{0x0, 0xF, 0x1, 0xA}})
+	check(directBucketEncoding{f: 5, b: 4}, bucket{l: 4, entries: [8]fingerprint{0x1C, 0x0F, 0x15, 0x1A}})
+	check(directBucketEncoding{f: 6, b: 4}, bucket{l: 4, entries: [8]fingerprint{0x2C, 0x0F, 0x35, 0x1A}})
+	check(directBucketEncoding{f: 8, b: 4}, bucket{l: 4, entries: [8]fingerprint{0x8C, 0x7D, 0x38, 0x44}})
 }
 
 func TestBasic(t *testing.T) {
-	f := New(20, 0.01)
+	f := NewRaw(4, 4, 7)
 	key := []byte{0x51}
 	require.Equal(t, f.Contains(key), No)
 	f.Add(key)
+	require.Equal(t, f.Contains(key), Maybe)
+
+	f.Add([]byte{0x77})
+	require.Equal(t, f.Contains(key), Maybe)
+
+	f.Add([]byte{0x19, 0x39})
 	require.Equal(t, f.Contains(key), Maybe)
 }
 
@@ -56,8 +85,11 @@ func testRandom(t *testing.T, r *rand.Rand, n int) {
 		require.Equal(t, Maybe, f.Contains(items[i]), "item %d broken", i)
 	}
 
+	//f.dump()
+	f.check()
+
 	for i := range items {
-		require.Equal(t, Maybe, f.Contains(items[i]))
+		require.Equal(t, Maybe, f.Contains(items[i]), "item %s missing", hex.EncodeToString(items[i]))
 	}
 
 	numFP := 0
